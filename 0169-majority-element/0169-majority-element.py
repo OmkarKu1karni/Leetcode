08aee413cedf0f1133e74107f9df45d0d956885b
OmkarKu1karni <165454978+OmkarKu1karni@users.noreply.github.com>
2024-08-17 07:06:43 +0530

    Time: 167 ms (72.47%), Space: 18 MB (72.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        D={}
-        for i in set(nums) :
-            D[i]=0
-        for i in nums :
-            D[i]+=1
+        el = None
         n = len(nums)
-        for el,count in D.items():
-            if count > n//2 :
-                return el
+        count = 0 
+        for i in range(n):
+            if count == 0 :
+                count = 1
+                el = nums[i]
+            elif nums[i] == el :
+                count += 1
+            else :
+                count -= 1
+        
+        count =0 
+        for i in nums :
+            if i==el : 
+                count+=1
+        if count > n//2:
+            return el
+        else :
+            return -1
+        
             
