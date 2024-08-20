692e003dc89c05715e520268b8d3e719c35924dd
OmkarKu1karni <165454978+OmkarKu1karni@users.noreply.github.com>
2024-08-20 08:04:41 +0530

    Time: 41 ms (89.07%), Space: 16.9 MB (87.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        D = defaultdict(int)
-        for i in s : 
-            D[i]+=1 
-        for i in t :
-            if i not in D.keys() :
-                return False 
-            else : 
-                D[i]-=1
-        for i,j in D.items(): 
-            if j!=0 :
-                return False
-        return True
+        
+        # D = defaultdict(int)
+        # for i in s : 
+        #     D[i]+=1 
+        # for i in t :
+        #     if i not in D.keys() :
+        #         return False 
+        #     else : 
+        #         D[i]-=1
+        # for i,j in D.items(): 
+        #     if j!=0 :
+        #         return False
+        # return True
+        
+        return all(s.count(x)==t.count(x) for x in set(s+t))
